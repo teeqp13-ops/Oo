86c2e75048d95178cf7f720ce3c9df5f7ac56e7f
teeqp13-ops <teeqp13@gmail.com>
2026-07-21 17:10:42 +0300

    إضافة إعداد مركزي لرابط API

---
+++ ActivationConfig.h
@@ -0,0 +1,9 @@
+#ifndef ActivationConfig_h
+#define ActivationConfig_h
+
+#define BYANO_API_BASE_URL @"https://key.p3nd.fun/api"
+#define BYANO_SETTINGS_ENDPOINT @"settings.php"
+#define BYANO_APP_NAME @"BYANO"
+#define BYANO_ACTIVATION_STORAGE_KEY @"BYANOActivationCode"
+
+#endif /* ActivationConfig_h */
