d711f33ca8ea8eac214fdecb0dc773b06dd629e2
teeqp13-ops <teeqp13@gmail.com>
2026-07-21 17:19:17 +0300

    استخدام activate.php كنقطة تفعيل رئيسية

--- ActivationConfig.h
+++ ActivationConfig.h
@@ -2,7 +2,7 @@
 #define ActivationConfig_h
 
 #define BYANO_API_BASE_URL @"https://key.p3nd.fun/api"
-#define BYANO_SETTINGS_ENDPOINT @"settings.php"
+#define BYANO_ACTIVATION_ENDPOINT @"activate.php"
 #define BYANO_APP_NAME @"BYANO"
 #define BYANO_ACTIVATION_STORAGE_KEY @"BYANOActivationCode"
 
